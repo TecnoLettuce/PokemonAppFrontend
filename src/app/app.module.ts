b78a1777bab9555909a5e96673c54c90f7a0c045
Gabrus <gabriel.sanchez@softwareone.com>
2021-09-30 15:42:27 +0200

    añadido componente de vista de tabla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableViewComponentComponent } from './table-view-component/table-view-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableViewComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/table-view-component/table-view-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-view-component',
+  templateUrl: './table-view-component.component.html',
+  styleUrls: ['./table-view-component.component.css']
+})
+export class TableViewComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
