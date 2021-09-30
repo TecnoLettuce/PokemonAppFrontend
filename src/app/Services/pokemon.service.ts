bbdc9f6578eefa7b5c694a345edd89f4076accea
Gabrus <gabriel.sanchez@softwareone.com>
2021-09-30 16:24:40 +0200

    creada logica del método get del crud

---
+++ src/app/Services/pokemon.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Pokemon } from '../Models/Pokemon';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  baseURL : string = 'https://localhost:44388/api/Pokemon'
+
+  constructor(private http : HttpClient) { 
+
+  }
+
+  getAllPokemon(): Observable<any> {
+
+    return this.http.get(this.baseURL)
+  }
+
+  getPokemonById(id : number): Observable<any> {
+
+    return this.http.get(this.baseURL + '/' + id)
+  }
+
+  createPokemon(pokemon : Pokemon) {
+    
+  }
+
+}
